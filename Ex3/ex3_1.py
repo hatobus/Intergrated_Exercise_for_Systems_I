6460a819a1beb02bea09c349f1323a0e6b6a8823
hatobus <s1240056@gmail.com>
2018-04-25 11:46:05 +0900

    [Update] ex3_1 add comment

--- Ex3/ex3_1.py
+++ Ex3/ex3_1.py
@@ -5,6 +5,7 @@ import time
 button = 25
 led = 24
 
+# Setting the GPIO state
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(led, GPIO.OUT, initial=GPIO.LOW)
@@ -12,7 +13,13 @@ GPIO.setup(led, GPIO.OUT, initial=GPIO.LOW)
 print("push button")
 
 while True:
+
+    # Lokking button pin state
     btn = GPIO.input(button)
+
+    # if GPIO 25's GPIO state is 1
+    # To blink LED 7 sec
+    # My student ID is s1240056
     if btn:
         print("Blink 7 sec")
         
