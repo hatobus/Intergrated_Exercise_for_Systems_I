0dbbdacf100b68a738123973cf28de8f4c7f8cb5
hatobus <s1240056@gmail.com>
2018-05-23 12:23:18 +0900

    [Update] Ex6

---
+++ Ex6/ex6/camera.py.ex
@@ -0,0 +1,18 @@
+import webiopi
+import os
+
+SAVEDIR = '/home/pi/ex6'
+
+@webiopi.macro
+def camera(fileno):
+    save_no = int(fileno)
+    path = SAVEDIR + '/camera_' + str(save_no) + '.jpg'
+
+    command = 'fswebcam -r 320x240 -d /dev/video0 ' + path
+    os.system(command)
+
+    os.system('sync')
+
+@webiopi.macro
+def gettemp(time)
+    
