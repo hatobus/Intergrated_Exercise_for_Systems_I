9601acc64863998e953f72374667dc221d3a9153
hatobus <s1240056@gmail.com>
2018-05-30 11:56:12 +0900

    [Update] Add to the time in webpage

--- Ex6/ex6/camera.py
+++ Ex6/ex6/camera.py
@@ -1,5 +1,6 @@
 import webiopi
 import os
+import datetime
 
 SAVEDIR = '/home/pi/ex6'
 
@@ -12,3 +13,28 @@ def camera(fileno):
     os.system(command)
 
     os.system('sync')
+
+@webiopi.macro
+def camera_time(fileno): 
+    save_no = int(fileno)
+    path = SAVEDIR + '/camera_' + str(save_no) + '.jpg'
+
+    command = 'fswebcam -r 320x240 -d /dev/video0 ' + path
+    os.system(command)
+
+    os.system('sync')
+
+    now = datetime.datetime.now()
+    took_time = ""
+
+    for d in [now.year, now.month, now.day]:
+        took_time += str(d) + "-"
+
+    took_time = took_time[:-1]
+    took_time += " "
+
+    for t in [now.hour, now.minute, now.second]:
+        took_time += str(t) + ":"
+
+    took_time = took_time[:-1]
+    return took_time
