2035d9dc9e1bcd9e00b87e03d54707a019bc8fda
hatobus <s1240056@gmail.com>
2018-07-04 12:03:54 +0900

    button

--- Ex7/tkinterpic.py
+++ Ex7/tkinterpic.py
@@ -2,25 +2,16 @@ import tkinter as Tk
 from PIL import Image, ImageTk
 
 
-class Application(Tk.Frame):
-    def __init__(self, master=None):
-        Tk.Frame.__init__(self, master)
-        self.pack()
-        self.createCanvas()
+def printpush(event):
+    print("Pushed")
 
-    def printpush(self, event):
-        print("Pushed")
-
-    def createCanvas(self):
-        root.title("Smart PhotoFrame")
-        root.geometry("400x300")
-        
-        #ボタン
-        Button = Tk.Button(text='ボタンです', width=50)
-        Button.bind("<Button-1>", printpush)
-        Button.pack()
-
-        
 root = Tk.Tk()
-app = Application(master=root)
-app.mainloop()
+root.title("Smart PhotoFrame")
+root.geometry("400x300")
+
+#ボタン
+Button = Tk.Button(text="change picture", width=50)
+Button.bind("<Button-1>", printpush)
+Button.pack()
+
+root.mainloop()
