22729d1deb3016a6fbcfa3730a85d23eaf4eeb60
hatobus <s1240056@gmail.com>
2018-07-15 01:19:01 +0900

    [Fix] 画像が重なって表示されないようにした

--- Ex7/tkinterpic.py
+++ Ex7/tkinterpic.py
@@ -1,21 +1,33 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+import random
 
 
 def displayPhoto(event):
-    im = Image.open("./downloadpic.png")
+    cv.delete("all")
+    pic = ["./color_bar.png", "./lena.png", "./downloadpic.png"]
+    im = Image.open(random.choice(pic))
     photo = ImageTk.PhotoImage(im)
     cv.create_image(10, 10, image=photo, anchor='nw')
     root.mainloop()
+
+def takePhoto(event):
+    print("hoge")
 
 root = tk.Tk()
 root.title("Smart Photo Frame") 
 root.geometry("1000x800")
 
-Button = tk.Button(text="change picture", width = 50)
-Button.bind("<Button-1>", displayPhoto)
-Button.pack()
+Buttonchange = tk.Button(text="change picture", width=50)
+Buttontake = tk.Button(text="take picture", width=50)
+
+Buttonchange.bind("<Button-1>", displayPhoto)
+Buttontake.bind("<Button-2>", takePhoto)
+
+Buttonchange.pack()
+Buttontake.pack()
 
 cv = tk.Canvas()
 cv.pack(side='top', fill='both', expand='yes')
+
 root.mainloop()
